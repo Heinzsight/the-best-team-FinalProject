589281194e94d95856c3dec8cddd23f12307930e
Makenzie Elliott <makenzie7elliott@gmail.com>
2018-06-27 15:08:43 -0400

    added customer order method

--- ElsevierProject/src/main/java/com/qa/services/CustomerOrderService.java
+++ ElsevierProject/src/main/java/com/qa/services/CustomerOrderService.java
@@ -1,4 +1,6 @@
 package com.qa.services;
+import com.qa.models.Customer;
+import com.qa.models.CustomerOrder;
 import com.qa.repositories.CustomerOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,7 @@ public class CustomerOrderService {
     @Autowired
     private CustomerOrderRepository customerOrderRepository;
 
-
+    public CustomerOrder getCustomerOrderByCustomer(Customer customer) {
+        return customerOrderRepository.getCustomerOrderByCustomer(customer);
+    }
 }
